17aeace25d30b7855f0becd18980f76aa0326646
Cyril Muchiri <cyrilmuchiri11@gmail.com>
2023-01-18 14:37:07 +0300

    Initial commit

--- src/Muchiri/Employee.java
+++ src/Muchiri/Employee.java
@@ -1,5 +1,7 @@
 package Muchiri;
 
+import java.util.Scanner;
+
 public class Employee {
 
     double a,b,c;
@@ -14,18 +16,18 @@ public class Employee {
     }
     double addSal(){
         if (a<500){
-            bonus+=10;
+            bonus=10;
         }else {
-            bonus=a;
+            bonus=0;
         }
        return bonus;
     }
 
     double addWork(){
         if (b>6){
-            bonus=+5;
+            bonus= 5;
         }else {
-            bonus=a;
+            bonus=0;
         }
 
         return bonus;
@@ -35,9 +37,15 @@ public class Employee {
 class Wage{
     public  static void main(String[] args){
         Employee employee=new Employee();
-        employee.a=200;
-        employee.b=8;
 
-        System.out.println(employee.addSal()+employee.addWork()+ employee.a);
+        System.out.println(" Enter Employees salary: ");
+        Scanner scan= new Scanner(System.in);
+        employee.a= scan.nextDouble();
+
+        System.out.println("Enter total no of hours: ");
+        employee.b= scan.nextDouble();
+
+        System.out.println((employee.addSal()+employee.addWork()) + employee.a);
+
     }
 }
